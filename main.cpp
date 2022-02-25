66a92faf2c4fef7dc7e724d16439197f0d51d2a7
misterEarly <afi.lovisfrueh@gmail.com>
2022-02-25 15:22:13 +0000

    added code to initialise and read from infrared sensors

--- main.cpp
+++ main.cpp
@@ -17,24 +17,17 @@ int main() {
   DigitalOut led4(PD_7);
   DigitalOut led5(PD_5);
 
+  DigitalOut enable(PG_1); // initialisieren vom sensor-wert
+  DigitalOut bit0(PF_0);   // multiplexer auswahlbit
+  DigitalOut bit1(PF_1);   // multiplexer auswahlbit
+  DigitalOut bit2(PF_2);   // multiplexer auswahlbit
+  enable = 1;              // schaltet die Sensoren ein
+
   while (true) {
-    led5 = 0;
-    led0 = 1;
-    ThisThread::sleep_for(BLINKING_RATE);
-    led0 = 0;
-    led1 = 1;
-    ThisThread::sleep_for(BLINKING_RATE);
-    led1 = 0;
-    led2 = 1;
-    ThisThread::sleep_for(BLINKING_RATE);
-    led2 = 0;
-    led3 = 1;
-    ThisThread::sleep_for(BLINKING_RATE);
-    led3 = 0;
-    led4 = 1;
-    ThisThread::sleep_for(BLINKING_RATE);
-    led4 = 0;
-    led5 = 1;
-    ThisThread::sleep_for(BLINKING_RATE);
+    bit0 = 1; // Wahl des Sensors mit dem Multiplexer (Sensor vorne)
+    bit1 = 1;
+    bit2 = 0;
+    float d = 0.09f / (distance + 0.001f) - 0.03f;   // Lesen der Distanz in [m]
+    printf("distance=%dmm\r\n", (int)(1000.0f * d)); // ausgeben der distanz
   }
 }
