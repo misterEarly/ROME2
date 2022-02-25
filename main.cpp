f264f94102d87a710e28d1545fe896d4fef23da7
misterEarly <afi.lovisfrueh@gmail.com>
2022-02-25 15:15:19 +0000

    added code to initialize LEDs and rotating lights

--- main.cpp
+++ main.cpp
@@ -5,18 +5,36 @@
 
 #include "mbed.h"
 
-
 // Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
+#define BLINKING_RATE 500ms
 
+int main() {
 
-int main()
-{
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led(LED1);
+  DigitalOut led0(PD_4);
+  DigitalOut led1(PD_3);
+  DigitalOut led2(PD_6);
+  DigitalOut led3(PD_2);
+  DigitalOut led4(PD_7);
+  DigitalOut led5(PD_5);
 
-    while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
-    }
+  while (true) {
+    led5 = 0;
+    led0 = 1;
+    ThisThread::sleep_for(BLINKING_RATE);
+    led0 = 0;
+    led1 = 1;
+    ThisThread::sleep_for(BLINKING_RATE);
+    led1 = 0;
+    led2 = 1;
+    ThisThread::sleep_for(BLINKING_RATE);
+    led2 = 0;
+    led3 = 1;
+    ThisThread::sleep_for(BLINKING_RATE);
+    led3 = 0;
+    led4 = 1;
+    ThisThread::sleep_for(BLINKING_RATE);
+    led4 = 0;
+    led5 = 1;
+    ThisThread::sleep_for(BLINKING_RATE);
+  }
 }
